31a6a7ed84c912bfe61c75f5dc4eb2cbc12eb40d
Cael <MR-Addict@qq.com>
2024-12-04 17:04:29 +0800

    feat: update bib syntax highlight

--- src/basic-languages/bib/bib.ts
+++ src/basic-languages/bib/bib.ts
@@ -6,39 +6,44 @@ import type { languages } from '../../fillers/monaco-editor-core';
 
 // .bib file is a bibliography file
 export const conf: languages.LanguageConfiguration = {
-	comments: {
-		lineComment: '%'
-	},
-	brackets: [
-		['{', '}'],
-		['[', ']'],
-		['(', ')']
-	],
+	brackets: [['{', '}']],
 	autoClosingPairs: [
 		{ open: '{', close: '}' },
-		{ open: '[', close: ']' },
-		{ open: '(', close: ')' },
 		{ open: '"', close: '"' }
 	],
 	surroundingPairs: [
 		{ open: '{', close: '}' },
-		{ open: '[', close: ']' },
-		{ open: '(', close: ')' },
 		{ open: '"', close: '"' }
 	]
 };
 
 export const language = <languages.IMonarchLanguage>{
-	defaultToken: '',
-	tokenPostfix: '.bib',
 	ignoreCase: true,
+	tokenPostfix: '.bib',
+
+	// Custom rules
+	brackets: [{ open: '{', close: '}', token: 'delimiter.curly' }],
 
 	// The main tokenizer for our languages
 	tokenizer: {
 		root: [
-			[/(%.*)/, 'comment'], // comments
-			[/@[a-zA-Z]+/, 'keyword'], // commands
-			[/\b\w+\b(?=\s*=)/, 'key'] // keys before '='
+			// Symbols, whitespace, and others
+			[/[=,]/, 'delimiter'],
+			[/[ \t\r\n]+/, 'white'],
+			[/[{}]/, '@brackets'],
+
+			// Bib entry types like @article, @book, @inproceedings
+			[/@[a-zA-Z]+/, 'keyword'],
+
+			// Bib citation key like @article{cite:key-2021, only cite:key-2021 part
+			[/[a-zA-Z0-9\-\:]+(?=\s*,)/, 'type.identifier'],
+
+			// Match field names like title, author, booktitle
+			[/[a-zA-Z]+(?=\s*=\s*)/, 'variable'],
+
+			// Match anything after the = sign, wrapped by quotes or curly brackets
+			[/".*?"/, 'string'],
+			[/[^=,{}"]+/, 'string']
 		]
 	}
 };
